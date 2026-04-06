cda9f42ed3d8a13a608e207b79dbca021cf36a62
Eric Lewis <ericlewis777@gmail.com>
2026-04-06 00:19:34 -0400

    Revert A5200 direct video clock experiment

--- pocket/pocket_pll.v
+++ pocket/pocket_pll.v
@@ -5,7 +5,6 @@
 // Outputs:
 //   outclk_0: 57.272728 MHz  (clk_sys)
 //   outclk_1: 114.545456 MHz (clk_mem, 180° phase for SDRAM)
-//   outclk_2: 57.272728 MHz 90° (clk_sys_90 for Pocket video DDR)
 
 `timescale 1 ps / 1 ps
 
@@ -14,7 +13,6 @@ module pll (
     input  wire        rst,
     output wire        outclk_0,
     output wire        outclk_1,
-    output wire        outclk_2,
     output wire        locked
 );
 
@@ -22,22 +20,19 @@ altera_pll #(
     .fractional_vco_multiplier ("true"),
     .reference_clock_frequency ("74.25 MHz"),
     .operation_mode            ("direct"),
-    .number_of_clocks          (3),
+    .number_of_clocks          (2),
     .output_clock_frequency0   ("57.272728 MHz"),
     .phase_shift0              ("0 ps"),
     .duty_cycle0               (50),
     .output_clock_frequency1   ("114.545456 MHz"),
     .phase_shift1              ("4365 ps"),
     .duty_cycle1               (50),
-    .output_clock_frequency2   ("57.272728 MHz"),
-    .phase_shift2              ("4365 ps"),
-    .duty_cycle2               (50),
     .pll_type                  ("General"),
     .pll_subtype               ("General")
 ) pll_inst (
     .refclk   ({1'b0, refclk}),
     .rst      (rst),
-    .outclk   ({outclk_2, outclk_1, outclk_0}),
+    .outclk   ({outclk_1, outclk_0}),
     .locked   (locked),
     .fboutclk (),
     .fbclk    (1'b0),
